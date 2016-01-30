fbc7b06156c8c62f7ad9b05f37850d052834f0db
Frank Boucher <fboucher@outlook.com>
2016-01-30 08:31:33 -0500

    Clean solution base

---
+++ MyClippingsAPIApp/MyClippingsAPIApp/Controllers/MyClippingsController.cs
@@ -0,0 +1,22 @@
+﻿using KindleClippings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyClippingsAPIApp.Controllers
+{
+    public class MyClippingsController : ApiController
+    {
+        [Route("GetAllFromFile")]
+        [HttpGet]
+        public IEnumerable<Clipping> GetAllFromFile(string filename)
+        {
+            var clippings = MyClippingsParser.Parse(filename);
+
+            return clippings;
+        }
+    }
+}
