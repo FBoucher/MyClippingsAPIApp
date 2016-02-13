d5c6fb403e317bafe99007dfa7a0b6be6024b058
Frank Boucher <fboucher@outlook.com>
2016-02-13 13:46:06 -0500

    - Rename methodes. - Add method to get only the latest

--- MyClippingsAPIApp/MyClippingsAPIApp/Controllers/MyClippingsController.cs
+++ MyClippingsAPIApp/MyClippingsAPIApp/Controllers/MyClippingsController.cs
@@ -10,13 +10,27 @@ namespace MyClippingsAPIApp.Controllers
 {
     public class MyClippingsController : ApiController
     {
-        [Route("GetAllFromFile")]
+        [Route("AllKindleClippings")]
         [HttpGet]
-        public IEnumerable<Clipping> GetAllFromFile(string filename)
+        public IEnumerable<Clipping> AllKindleClippings(string filename)
         {
             var clippings = MyClippingsParser.Parse(filename);
 
             return clippings;
         }
+
+        [Route("KindleClippingsAfter")]
+        [HttpGet]
+        public IEnumerable<Clipping> KindleClippingsAfter(string filename, string StartDate)
+        {
+            var clippings = KindleClippings.MyClippingsParser.Parse(filename);
+
+            var result = from c in clippings
+                         where c.DateAdded >= DateTime.Parse(StartDate)
+                         select c as KindleClippings.Clipping;
+
+            return result;
+        }
+
     }
 }
